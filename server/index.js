7c78b145449e141f73883a1f6d91f529da1b2bc2
NeeJed <belugin-denis2@rambler.ru>
2024-03-28 21:31:52 +0300

    fixing cors 3

--- server/index.js
+++ server/index.js
@@ -17,6 +17,12 @@ app.use(fileUpload({}));
 app.use(cors());
 app.use('/api', router);
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
+header("Access-Control-Max-Age", "3600");
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header("Access-Control-Allow-Credentials", "true");
+
 // error last midl
 app.use(errorHandler);
 
