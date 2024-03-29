b7c6585c40ef01b60f5fdfa6f67fd8d57089b412
NeeJed <belugin-denis2@rambler.ru>
2024-03-29 10:23:30 +0300

    change vercel static

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(express.static(path.resolve(__dirname, 'tmp/static')))
 app.use(fileUpload({}));
 app.use(cors());
 app.use('/api', router);
